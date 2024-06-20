aa124ef32bd255626b72fa4df0bec5aeb3b01727
matiz22 <mateuszmalich@gmail.com>
2024-06-20 13:57:30 +0200

    corrected type in cocktails repository

--- shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
@@ -1,13 +1,14 @@
 package pl.matiz22.cocktailapp.cocktails.domain.repository
 
 import pl.matiz22.cocktailapp.cocktails.domain.model.Drink
+import pl.matiz22.cocktailapp.cocktails.domain.model.Drinks
 import pl.matiz22.cocktailapp.root.domain.DataError
 import pl.matiz22.cocktailapp.root.domain.Result
 
 interface DrinksRepository {
-    suspend fun getDrinksByName(query: String): Result<List<Drink>, DataError.Network>
-    suspend fun getDrinksByFirstLetter(letter: Char): Result<List<Drink>, DataError.Network>
+    suspend fun getDrinksByName(query: String): Result<Drinks, DataError.Network>
+    suspend fun getDrinksByFirstLetter(letter: Char): Result<Drinks, DataError.Network>
     suspend fun getDrinkById(id: String): Result<Drink, DataError.Network>
     suspend fun getRandomDrink(): Result<Drink, DataError.Network>
-    suspend fun getDrinkByIngredient(ingredient: String): Result<List<Drink>, DataError>
+    suspend fun getDrinkByIngredient(ingredient: String): Result<Drinks, DataError.Network>
 }
