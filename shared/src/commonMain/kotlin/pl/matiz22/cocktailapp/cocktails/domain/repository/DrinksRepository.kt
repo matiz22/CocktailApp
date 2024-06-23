0ca7cd1e009527c3f6c8b430d4c0bbdacfd18254
matiz22 <mateuszmalich@gmail.com>
2024-06-23 22:05:56 +0200

    added nullable types

--- shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
@@ -9,7 +9,7 @@ import pl.matiz22.cocktailapp.root.domain.Result
 interface DrinksRepository {
     suspend fun getDrinksByName(query: String): Result<Drinks, DataError.Network>
     suspend fun getDrinksByFirstLetter(letter: Char): Result<Drinks, DataError.Network>
-    suspend fun getDrinkById(id: String): Result<Drink, DataError.Network>
-    suspend fun getRandomDrink(): Result<Drink, DataError.Network>
+    suspend fun getDrinkById(id: String): Result<Drink?, DataError.Network>
+    suspend fun getRandomDrink(): Result<Drink?, DataError.Network>
     suspend fun getDrinksByIngredient(ingredient: String): Result<DrinksSummary, DataError.Network>
 }
