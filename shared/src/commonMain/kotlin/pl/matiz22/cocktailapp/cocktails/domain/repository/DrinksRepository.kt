1194b00b91a51101f4b1a424a34f095ec145078c
matiz22 <mateuszmalich@gmail.com>
2024-06-23 21:06:29 +0200

    added DrinksSummary to repository

--- shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/domain/repository/DrinksRepository.kt
@@ -2,6 +2,7 @@ package pl.matiz22.cocktailapp.cocktails.domain.repository
 
 import pl.matiz22.cocktailapp.cocktails.domain.model.Drink
 import pl.matiz22.cocktailapp.cocktails.domain.model.Drinks
+import pl.matiz22.cocktailapp.cocktails.domain.model.DrinksSummary
 import pl.matiz22.cocktailapp.root.domain.DataError
 import pl.matiz22.cocktailapp.root.domain.Result
 
@@ -10,5 +11,5 @@ interface DrinksRepository {
     suspend fun getDrinksByFirstLetter(letter: Char): Result<Drinks, DataError.Network>
     suspend fun getDrinkById(id: String): Result<Drink, DataError.Network>
     suspend fun getRandomDrink(): Result<Drink, DataError.Network>
-    suspend fun getDrinkByIngredient(ingredient: String): Result<Drinks, DataError.Network>
+    suspend fun getDrinksByIngredient(ingredient: String): Result<DrinksSummary, DataError.Network>
 }
