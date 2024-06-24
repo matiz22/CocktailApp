7058383da456f8700561b3f44424bb65f517e3f4
matiz22 <mateuszmalich@gmail.com>
2024-06-24 15:31:29 +0200

    CocktailsDataModule in common

---
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/di/cocktailsDataModule.kt
@@ -0,0 +1,17 @@
+package pl.matiz22.cocktailapp.cocktails.di
+
+import org.koin.dsl.module
+import pl.matiz22.cocktailapp.cocktails.data.remote.repository.DrinksRepositoryImpl
+import pl.matiz22.cocktailapp.cocktails.data.remote.source.CocktailsDbApi
+import pl.matiz22.cocktailapp.cocktails.data.remote.source.httpClient
+import pl.matiz22.cocktailapp.cocktails.data.remote.util.defaultHttpClientConfig
+import pl.matiz22.cocktailapp.cocktails.domain.repository.DrinksRepository
+
+fun cocktailsDataModule() = module {
+    single {
+        CocktailsDbApi(httpClient = httpClient(config = defaultHttpClientConfig))
+    }
+    single<DrinksRepository> {
+        DrinksRepositoryImpl(get())
+    }
+}
