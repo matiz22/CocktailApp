344784b3aa136479ecfc6a7b45656e87448de794
matiz22 <mateuszmalich@gmail.com>
2024-06-24 17:37:42 +0200

    implementation content negotation

--- shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/root/data/util/provideDataErrorNetworkForException.kt
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/root/data/util/provideDataErrorNetworkForException.kt
@@ -1,11 +1,13 @@
 package pl.matiz22.cocktailapp.root.data.util
 
 import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.serialization.JsonConvertException
 import pl.matiz22.cocktailapp.root.domain.DataError
 
 fun provideDataErrorNetworkForException(exception: Exception): DataError.Network {
     return when (exception) {
         is NoTransformationFoundException -> DataError.Network.SERIALIZATION
+        is JsonConvertException -> DataError.Network.SERIALIZATION
         else -> DataError.Network.NO_INTERNET // no access to java.net.UnknownHostException
     }
 }
