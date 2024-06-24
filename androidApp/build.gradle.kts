71d1e10e69a85e4e8051181248effc598265fe68
matiz22 <mateuszmalich@gmail.com>
2024-06-24 19:56:59 +0200

    google font lib

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -42,5 +42,6 @@ dependencies {
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.ui.text.google.fonts)
     debugImplementation(libs.compose.ui.tooling)
 }
