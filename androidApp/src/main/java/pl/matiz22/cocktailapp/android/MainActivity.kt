5b70b8a36813daf04e85fa9929221d46f5ba26f4
matiz22 <mateuszmalich@gmail.com>
2024-07-04 18:01:28 +0200

    Added BottomBar and NavHost to Scaffold

--- androidApp/src/main/java/pl/matiz22/cocktailapp/android/MainActivity.kt
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/MainActivity.kt
@@ -4,20 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import pl.matiz22.cocktailapp.android.core.presentation.composables.bottombar.BottomBar
+import pl.matiz22.cocktailapp.android.core.presentation.navigation.route.AppRoutes
+import pl.matiz22.cocktailapp.android.core.presentation.navigation.util.navItems
+import pl.matiz22.cocktailapp.android.favourites.presentation.graph.favouritesGraph
+import pl.matiz22.cocktailapp.android.home.presentation.graph.homeGraph
+import pl.matiz22.cocktailapp.android.search.presentation.graph.searchGraph
 import pl.matiz22.cocktailapp.android.theme.CocktailsAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             CocktailsAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = CocktailsAppTheme.colors.background
                 ) {
-
+                    Scaffold(
+                        bottomBar = {
+                            BottomBar(
+                                navItems = navItems(navController)
+                            )
+                        }
+                    ) { paddingValues ->
+                        NavHost(
+                            modifier = Modifier.padding(paddingValues),
+                            navController = navController,
+                            startDestination = AppRoutes.Home
+                        ) {
+                            homeGraph(navController)
+                            searchGraph(navController)
+                            favouritesGraph(navController)
+                        }
+                    }
                 }
             }
         }
