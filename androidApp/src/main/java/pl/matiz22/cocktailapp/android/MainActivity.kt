77a3f7249a4d96a7a49bbcf19fa66b7fb9893ba2
matiz22 <mateuszmalich@gmail.com>
2024-07-08 16:47:31 +0200

    Added AppBar to Scaffold

--- androidApp/src/main/java/pl/matiz22/cocktailapp/android/MainActivity.kt
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/MainActivity.kt
@@ -7,12 +7,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import io.github.skeptick.libres.compose.painterResource
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import pl.matiz22.cocktailapp.SharedRes
+import pl.matiz22.cocktailapp.android.core.presentation.composables.appbar.AppBar
+import pl.matiz22.cocktailapp.android.core.presentation.composables.appbar.TitleAndDescription
 import pl.matiz22.cocktailapp.android.core.presentation.composables.bottombar.BottomBar
+import pl.matiz22.cocktailapp.android.core.presentation.composables.iconbutton.AppIconButton
 import pl.matiz22.cocktailapp.android.core.presentation.navigation.route.AppRoutes
 import pl.matiz22.cocktailapp.android.core.presentation.navigation.util.navItems
+import pl.matiz22.cocktailapp.android.core.presentation.viewmodels.ScaffoldViewModel
 import pl.matiz22.cocktailapp.android.favourites.presentation.graph.favouritesGraph
 import pl.matiz22.cocktailapp.android.home.presentation.graph.homeGraph
 import pl.matiz22.cocktailapp.android.search.presentation.graph.searchGraph
@@ -23,6 +32,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
+            val scaffoldViewModel = getViewModel<ScaffoldViewModel>()
+            navController.addOnDestinationChangedListener(listener = scaffoldViewModel::updateAppBar)
+            val appBarTextsState by scaffoldViewModel.appBarTextsState.collectAsState()
             CocktailsAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -32,6 +44,25 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .fillMaxSize()
                             .background(CocktailsAppTheme.colors.background),
+                        topBar = {
+                            AppBar(
+                                leftSideContent = {
+                                    if (appBarTextsState.navigateBack) {
+                                        AppIconButton(
+                                            painter = SharedRes.image.arrow_left.painterResource(),
+                                            contentDescription = SharedRes.string.nav_navigate_back_description,
+                                            onClick = {
+                                                navController.navigateUp()
+                                            }
+                                        )
+                                    }
+                                    TitleAndDescription(
+                                        title = appBarTextsState.name,
+                                        description = appBarTextsState.description
+                                    )
+                                }
+                            )
+                        },
                         bottomBar = {
                             BottomBar(
                                 navItems = navItems(navController)
