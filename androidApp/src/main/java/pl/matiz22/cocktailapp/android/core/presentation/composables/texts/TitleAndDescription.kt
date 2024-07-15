9652ad8ed43424999da7ce9deaf99adf6de55abc
matiz22 <mateuszmalich@gmail.com>
2024-07-15 14:20:49 +0200

    Fixed color font

--- androidApp/src/main/java/pl/matiz22/cocktailapp/android/core/presentation/composables/texts/TitleAndDescription.kt
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/core/presentation/composables/texts/TitleAndDescription.kt
@@ -25,7 +25,7 @@ fun TitleAndDescription(
             Text(
                 text = description,
                 style = CocktailsAppTheme.typography.paragraphLarge,
-                color = CocktailsAppTheme.colors.font
+                color = CocktailsAppTheme.colors.fontLight
             )
         }
     }
