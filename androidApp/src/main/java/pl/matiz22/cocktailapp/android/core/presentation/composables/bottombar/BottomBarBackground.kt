08e1e1543dc7c7902b58ab4bd20b846ffdf31fef
matiz22 <mateuszmalich@gmail.com>
2024-07-15 12:26:09 +0200

    Fixed clickable items under bottom bar

--- androidApp/src/main/java/pl/matiz22/cocktailapp/android/core/presentation/composables/bottombar/BottomBarBackground.kt
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/core/presentation/composables/bottombar/BottomBarBackground.kt
@@ -3,18 +3,22 @@ package pl.matiz22.cocktailapp.android.core.presentation.composables.bottombar
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.Color
 import pl.matiz22.cocktailapp.android.theme.CocktailsAppTheme
 
 @Composable
 fun BottomBarBackground(
     modifier: Modifier = Modifier,
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    Box(modifier = modifier) {
+    Surface(
+        modifier = modifier,
+        color = Color.Transparent
+    ) {
         if (isSystemInDarkTheme()) {
             Box(
                 modifier = Modifier
