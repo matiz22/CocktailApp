e096960a3a3c0b11e472f6e579f9d832405f34a9
matiz22 <mateuszmalich@gmail.com>
2024-07-15 11:50:11 +0200

    AppScaffold in FavouritesScreen

--- androidApp/src/main/java/pl/matiz22/cocktailapp/android/favourites/presentation/graph/FavouritesGraph.kt
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/favourites/presentation/graph/FavouritesGraph.kt
@@ -4,7 +4,13 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
+import pl.matiz22.cocktailapp.SharedRes
+import pl.matiz22.cocktailapp.android.core.presentation.composables.appbar.AppBar
+import pl.matiz22.cocktailapp.android.core.presentation.composables.appscaffold.AppScaffold
+import pl.matiz22.cocktailapp.android.core.presentation.composables.bottombar.BottomBar
+import pl.matiz22.cocktailapp.android.core.presentation.composables.texts.TitleAndDescription
 import pl.matiz22.cocktailapp.android.core.presentation.navigation.route.AppRoutes
+import pl.matiz22.cocktailapp.android.core.presentation.navigation.util.navItems
 import pl.matiz22.cocktailapp.android.favourites.presentation.screens.FavouritesScreen
 
 fun NavGraphBuilder.favouritesGraph(
@@ -12,7 +18,25 @@ fun NavGraphBuilder.favouritesGraph(
 ) {
     navigation<AppRoutes.Favourites>(startDestination = AppRoutes.Favourites.FavouritesScreen) {
         composable<AppRoutes.Favourites.FavouritesScreen> {
-            FavouritesScreen()
+            AppScaffold(
+                topAppbar = {
+                    AppBar(
+                        leftSideContent = {
+                            TitleAndDescription(
+                                title = SharedRes.string.nav_favourite,
+                                description = SharedRes.string.nav_favourite_favourite_screen_description
+                            )
+                        }
+                    )
+                },
+                bottomAppbar = {
+                    BottomBar(
+                        navItems = navItems(navController = navController)
+                    )
+                }
+            ) {
+                FavouritesScreen()
+            }
         }
     }
 }
