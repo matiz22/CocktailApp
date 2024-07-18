79b5483241330380d725b55075b5f4f18dc52f54
matiz22 <mateuszmalich@gmail.com>
2024-07-18 10:30:32 +0200

    Create DrinkInfo.kt

---
+++ androidApp/src/main/java/pl/matiz22/cocktailapp/android/drinks/presentation/composables/DrinkInfo.kt
@@ -0,0 +1,37 @@
+package pl.matiz22.cocktailapp.android.drinks.presentation.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import pl.matiz22.cocktailapp.android.core.presentation.composables.texts.TitleAndDescription
+import pl.matiz22.cocktailapp.android.theme.CocktailsAppTheme
+import pl.matiz22.cocktailapp.cocktails.domain.model.Drink
+
+@Composable
+fun DrinkInfo(
+    modifier: Modifier = Modifier,
+    drink: Drink
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        TitleAndDescription(
+            title = drink.name,
+            description = drink.category
+        )
+        Text(
+            text = drink.alcoholic,
+            style = CocktailsAppTheme.typography.paragraphLarge,
+            color = CocktailsAppTheme.colors.fontLight
+        )
+        Text(
+            text = drink.glass,
+            style = CocktailsAppTheme.typography.paragraphLarge,
+            color = CocktailsAppTheme.colors.fontLight
+        )
+    }
+}
