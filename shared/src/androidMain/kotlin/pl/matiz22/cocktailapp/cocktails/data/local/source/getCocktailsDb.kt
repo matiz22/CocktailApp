ca85b233da780616b6c86fd1ba69e9ba8af26ab2
matiz22 <mateuszmalich@gmail.com>
2024-07-20 13:26:11 +0200

    Cocktails db provider for specific platform

---
+++ shared/src/androidMain/kotlin/pl/matiz22/cocktailapp/cocktails/data/local/source/getCocktailsDb.kt
@@ -0,0 +1,14 @@
+package pl.matiz22.cocktailapp.cocktails.data.local.source
+
+import android.content.Context
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+
+fun getCocktailsDb(context: Context): CocktailsLocalDb {
+    val appContext = context.applicationContext
+    val dbFile = appContext.getDatabasePath("cocktails.db")
+    return Room.databaseBuilder<CocktailsLocalDb>(
+        context = appContext,
+        name = dbFile.absolutePath
+    ).setDriver(BundledSQLiteDriver()).build()
+}
---
+++ shared/src/iosMain/kotlin/pl/matiz22/cocktailapp/cocktails/data/local/source/getCocktailsDb.kt
@@ -0,0 +1,13 @@
+package pl.matiz22.cocktailapp.cocktails.data.local.source
+
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import platform.Foundation.NSHomeDirectory
+
+fun getCocktailsDb(): CocktailsLocalDb {
+    val dbFile = NSHomeDirectory() + "/cocktails.db"
+    return Room.databaseBuilder<CocktailsLocalDb>(
+        name = dbFile,
+        factory = { CocktailsLocalDb::class.instantiateImpl() }
+    ).setDriver(BundledSQLiteDriver()).build()
+}
