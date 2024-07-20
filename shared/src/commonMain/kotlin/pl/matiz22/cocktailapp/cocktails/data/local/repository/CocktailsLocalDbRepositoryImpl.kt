5b89739a0515ab543a935a10d5153e5d01de1936
matiz22 <mateuszmalich@gmail.com>
2024-07-20 13:27:17 +0200

    Impl local cocktails repo

---
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/data/local/repository/CocktailsLocalDbRepositoryImpl.kt
@@ -0,0 +1,55 @@
+package pl.matiz22.cocktailapp.cocktails.data.local.repository
+
+import pl.matiz22.cocktailapp.cocktails.data.local.dao.CocktailDao
+import pl.matiz22.cocktailapp.cocktails.data.local.source.CocktailsLocalDb
+import pl.matiz22.cocktailapp.cocktails.domain.model.Drink
+import pl.matiz22.cocktailapp.cocktails.domain.model.Drinks
+import pl.matiz22.cocktailapp.cocktails.domain.model.Ingredient
+import pl.matiz22.cocktailapp.cocktails.domain.model.IngredientsAndMeasures
+import pl.matiz22.cocktailapp.cocktails.domain.model.Measure
+import pl.matiz22.cocktailapp.cocktails.domain.repository.local.CocktailsLocalDbRepository
+import pl.matiz22.cocktailapp.root.domain.model.DataError
+import pl.matiz22.cocktailapp.root.domain.model.Result
+
+class CocktailsLocalDbRepositoryImpl(
+    private val cocktailDao: CocktailDao
+) : CocktailsLocalDbRepository {
+    override suspend fun getDrinks(): Result<Drinks, DataError> {
+        return try {
+            val drinks = cocktailDao.getDrinks().map { drinkWithIngredients ->
+                Drink(
+                    id = drinkWithIngredients.drinkEntity.id,
+                    name = drinkWithIngredients.drinkEntity.name,
+                    category = drinkWithIngredients.drinkEntity.category,
+                    instructions = drinkWithIngredients.drinkEntity.instructions,
+                    alcoholic = drinkWithIngredients.drinkEntity.alcoholic,
+                    glass = drinkWithIngredients.drinkEntity.glass,
+                    image = drinkWithIngredients.drinkEntity.image,
+                    ingredientsAndMeasures = IngredientsAndMeasures(
+                        drinkWithIngredients.ingredientsEntity.associate { ingredientsEntity ->
+                            Ingredient(
+                                id = ingredientsEntity.id,
+                                name = ingredientsEntity.ingredientName
+                            ) to Measure(
+                                ingredientsEntity.ingredientName
+                            )
+                        }
+                    ),
+                    liked = drinkWithIngredients.drinkEntity.liked
+                )
+            }
+            Result.Success(Drinks(drinks))
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
+    }
+
+    override suspend fun saveDrink(drink: Drink): Result<Boolean, DataError> {
+        return try {
+            cocktailDao.upsertDrinkWithIngredients(drink)
+            Result.Success(true)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.DATABASE_ERROR)
+        }
+    }
+}
