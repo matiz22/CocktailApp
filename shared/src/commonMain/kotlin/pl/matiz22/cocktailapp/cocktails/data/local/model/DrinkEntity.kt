baf455324786f8ec9d9ab0bd2fe859802a1e1caa
matiz22 <mateuszmalich@gmail.com>
2024-07-20 13:22:18 +0200

    Create DrinkEntity.kt

---
+++ shared/src/commonMain/kotlin/pl/matiz22/cocktailapp/cocktails/data/local/model/DrinkEntity.kt
@@ -0,0 +1,17 @@
+package pl.matiz22.cocktailapp.cocktails.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Drinks")
+data class DrinkEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val category: String,
+    val alcoholic: String,
+    val glass: String,
+    val instructions: String,
+    val image: String,
+    val liked: Boolean
+)
