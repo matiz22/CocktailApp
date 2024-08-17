430a3eec1e6e01916bfc2d940027f9d4ad017ad3
matiz22 <mateuszmalich@gmail.com>
2024-08-17 15:13:18 +0200

    ios local db koin module

---
+++ shared/src/iosMain/kotlin/pl/matiz22/cocktailapp/cocktails/di/cocktailsDataModule.ios.kt
@@ -0,0 +1,8 @@
+package pl.matiz22.cocktailapp.cocktails.di
+
+import org.koin.dsl.module
+import pl.matiz22.cocktails.data.local.source.getCocktailsDb
+
+fun cocktailsLocalDataModule() = module {
+    single { getCocktailsDb() }
+}
