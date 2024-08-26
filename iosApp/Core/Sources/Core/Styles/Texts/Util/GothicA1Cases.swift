0a923a5d3dc05a9c50a7a704641d077c55a6c14f
matiz22 <mateuszmalich@gmail.com>
2024-08-26 22:43:05 +0200

    added missing cases of GothicA1 font

--- iosApp/Core/Sources/Core/Styles/Texts/Util/GothicA1Cases.swift
+++ iosApp/Core/Sources/Core/Styles/Texts/Util/GothicA1Cases.swift
@@ -1,8 +1,11 @@
 public enum GothicA1Cases: String, CaseIterable {
+	case black = "GothicA1-Black"
 	case regular = "GothicA1-Regular"
 	case medium = "GothicA1-Medium"
 	case bold = "GothicA1-Bold"
 	case extraBold = "GothicA1-ExtraBold"
+	case extraLight = "GothicA1-ExtraLight"
 	case light = "GothicA1-Light"
+	case semiBold = "GothicA1-SemiBold"
 	case thin = "GothicA1-Thin"
 }
