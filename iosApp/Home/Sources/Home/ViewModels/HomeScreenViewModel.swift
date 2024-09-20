91b20dd466fb7975a08fc8c9ec0691ec0b18a7ec
matiz22 <mateuszmalich@gmail.com>
2024-09-20 15:55:10 +0200

    adding ObservableObject and correcting async func

--- iosApp/Home/Sources/Home/ViewModels/HomeScreenViewModel.swift
+++ iosApp/Home/Sources/Home/ViewModels/HomeScreenViewModel.swift
@@ -1,23 +1,29 @@
 import Core
 import Shared
 
-public class HomeScreenViewModel {
+public class HomeScreenViewModel: ObservableObject {
 	private let drinksLocalRepository: DrinksLocalRepository = DrinksLocalRepositoryProvider().drinksLocalRepository
 	@Published private(set) var recentDrinks: DataState<Drinks> = .loading
 
 	public init() {}
 
 	func fetchDrinks() async {
-		recentDrinks = .loading
+		await MainActor.run {
+			recentDrinks = .loading
+		}
 		do {
 			let drinksData = try await drinksLocalRepository.getRecentDrinks()
-			if let data = drinksData.data {
-				recentDrinks = .success(data: data)
-			} else if let error = drinksData.error {
-				recentDrinks = .error(error: String(reflecting: error))
+			await MainActor.run {
+				if let data = drinksData.data {
+					recentDrinks = .success(data: data)
+				} else if let error = drinksData.error {
+					recentDrinks = .error(error: String(reflecting: error))
+				}
 			}
 		} catch {
-			recentDrinks = .error(error: String(localized: "UNKNOWN", bundle: .CoreBundle))
+			await MainActor.run {
+				recentDrinks = .error(error: String(localized: "UNKNOWN", bundle: .CoreBundle))
+			}
 		}
 	}
 }
