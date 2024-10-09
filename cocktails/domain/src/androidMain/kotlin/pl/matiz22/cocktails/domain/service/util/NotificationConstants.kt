4ba3da797bacf0b40a615d35f35e36254df7f102
matiz22 <mateuszmalich@gmail.com>
2024-10-09 14:06:51 +0200

    Create NotificationConstants.kt

---
+++ cocktails/domain/src/androidMain/kotlin/pl/matiz22/cocktails/domain/service/util/NotificationConstants.kt
@@ -0,0 +1,6 @@
+package pl.matiz22.cocktails.domain.service.util
+
+object NotificationConstants {
+    const val RANDOM_DRINK_ID = "RANDOM_DRINK"
+    const val RANDOM_DRINK_NAME = "Daily drink"
+}
